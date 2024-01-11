170c9a66e0712ebd14937992b6f100a25a024120
vipink28 <vipinkalyan28@gmail.com>
2024-01-11 11:31:58 +0530

    project initialised

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+    var title = "React Learning"
+    var bgPrimary = "bg-primary";
+    var isHeader = true;
+
+    var styleObj = {
+        backgroundColor: "red",
+        fontSize: 50 + "%"
+    }
+
+    // condition ? true statement:falsestatement
+
+    return (
+        <div className={isHeader ? "bg-primary" : "bg-secondary"}>
+            <h2>Header Component</h2>
+            <h3 style={styleObj}>{title}</h3>
+        </div>
+    )
+}
+export default Header;
